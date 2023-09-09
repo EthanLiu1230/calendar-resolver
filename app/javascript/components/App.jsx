b9791b26934ea5bcb8773952a1da88b7a80fd61a
Ethan Liu <1230lyx@gmail.com>
2023-09-09 15:29:34 +0800

    Make tailwindcss work with react jsx

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+
+function DraggableEvent ({
+  id,
+  event = {
+    title: 'my event', duration: '48:00',
+  },
+  children,
+}) {
+  const eventId = `event-${id}`;
+  useEffect(() => {
+    new Draggable(document.querySelector(`#${eventId}`), { eventData: event });
+  }, []);
+  return (<div id={eventId} className="draggable-el">{children}</div>);
+}
+
+function ExternalEventBox ({ children }) {
+  return (
+    <div className="bg-blue-500 text-white rounded">{children}</div>
+  );
+}
 
 function App () {
-  const [count, setCount] = useState(0);
-  return (<>
-      <p>you clicked {count} times!</p>
-      <button onClick={() => setCount(count + 1)}>add</button>
-    </>
 
+  return (
+    <main className={'container'}>
+      <DraggableEvent id={'fifa'} event={{
+        title: 'FIFA',
+        duration: '72:00',
+      }}><ExternalEventBox>FIFA</ExternalEventBox></DraggableEvent>
+
+      <DraggableEvent id={'world_cup'}
+                      event={{ title: 'World Cup', duration: '49:00' }}><ExternalEventBox>World
+        Cup</ExternalEventBox></DraggableEvent>
+
+      <div className="calendar-container">
+        <FullCalendar
+          plugins={[
+            dayGridPlugin,
+            interactionPlugin,
+            timeGridPlugin,
+          ]}
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek',
+          }}
+          initialView="dayGridMonth"
+          nowIndicator={true}
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          initialEvents={[
+            { title: 'nice event', start: new Date(), resourceId: 'a' },
+          ]}
+        />
+      </div>
+    </main>
   );
 }
 
